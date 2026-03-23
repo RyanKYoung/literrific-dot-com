fa6e2bcc10f1f28557f4c46fdcea01c4c319acdf
RyanKYoung <ryan@literrific.com>
2026-03-23 16:23:08 -0500

    test

--- publish.js
+++ publish.js
@@ -1,2 +1,27 @@
-node_modules/
-.env
+name: Publish to Webflow CMS
+
+on:
+  push:
+    paths:
+      - 'posts/**'
+    branches:
+      - main
+
+jobs:
+  publish:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v4
+
+      - uses: actions/setup-node@v4
+        with:
+          node-version: '20'
+
+      - name: Install dependencies
+        run: npm ci
+
+      - name: Publish posts to Webflow
+        env:
+          WEBFLOW_API_TOKEN: ${{ secrets.WEBFLOW_API_TOKEN }}
+          WEBFLOW_COLLECTION_ID: ${{ secrets.WEBFLOW_COLLECTION_ID }}
+        run: node scripts/publish.js
